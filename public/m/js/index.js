576b81e4aa7f3d556b7b9b1ce6a64b67cf24e77c
liushilin001 <liushilin001@126.com>
2018-09-03 21:48:36 +0800

    乐淘第一次上传

---
+++ public/m/js/index.js
@@ -0,0 +1,30 @@
+
+$(function () {
+    var letao = new Letao();
+    letao.initSlide();
+    letao.initScroll();
+});
+
+var Letao = function () {
+
+}
+
+Letao.prototype = {
+    initSlide: function () {
+        var gallery = mui('.mui-slider');
+        gallery.slider({
+            interval: 1000 //自动轮播周期，若为0则不自动播放，默认为0；
+        });
+    },
+    initScroll: function () {
+        mui('.mui-scroll-wrapper').scroll({
+            scrollY: true, //是否竖向滚动
+            scrollX: false, //是否横向滚动
+            startX: 0, //初始化时滚动至x
+            startY: 0, //初始化时滚动至y
+            indicators: true, //是否显示滚动条
+            deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+            bounce: true //是否启用回弹
+        });
+    }
+}
